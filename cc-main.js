8011b450f2815ed761c215665f8b38f8798a89e6
AndKorbiel <andkorbiel@gmail.com>
2018-08-29 15:26:22 +0200

    STAGE: new feature added: checking if game is tied

--- cc-main.js
+++ cc-main.js
@@ -51,6 +51,7 @@ document.body.addEventListener("click", function(e) {
             targ.innerHTML = 'X';   // point X at table cell
             targ.classList.add("taken");
 
+              // winning conditionals for player
               if ((a1.innerHTML === 'X' && a2.innerHTML === 'X' && a3.innerHTML === 'X') || (b1.innerHTML === 'X' && b2.innerHTML === 'X' && b3.innerHTML === 'X') || (c1.innerHTML === 'X' && c2.innerHTML === 'X' && c3.innerHTML === 'X') || (a1.innerHTML === 'X' && b2.innerHTML === 'X' && c3.innerHTML === 'X') || (a3.innerHTML === 'X' && b2.innerHTML === 'X' && c1.innerHTML === 'X') || (a1.innerHTML === 'X' && b1.innerHTML === 'X' && c1.innerHTML === 'X') || (a2.innerHTML === 'X' && b2.innerHTML === 'X' && c2.innerHTML === 'X') || (a3.innerHTML === 'X' && b3.innerHTML === 'X' && c3.innerHTML === 'X')
               )
 
@@ -62,7 +63,15 @@ document.body.addEventListener("click", function(e) {
                       }) 
                    }
 
-              else { }
+              // check if every piece of board is taken 
+              else if (boardTds.every(element => element.classList.contains('taken')) ) {
+                  document.getElementById("reset").style.opacity = 1;
+                  alert('Tie!');
+              }
+
+              else {
+              compPick();
+              }
 
           }
 
@@ -70,4 +79,41 @@ document.body.addEventListener("click", function(e) {
 
      else { }
 
+     function compPick() {
+          // pick up random elment from array
+          let rand = board[Math.floor(Math.random() * board.length)];
+             
+             // check if fiels isn't already taken 
+             if (!(document.getElementById(rand).classList.contains("taken"))) { 
+             
+                 document.getElementById(rand).innerHTML = 'O';
+                 document.getElementById(rand).classList.add("taken");
+
+                 // reasigning index value to new ID and than remove it
+                 index = board.indexOf(rand);
+                  
+                 // if array is not empty  
+                 if (index !== -1) board.splice(index, 1);
+                        
+                      // winning conditionals for CPU 
+                      if ((a1.innerHTML === 'O' && a2.innerHTML === 'O' && a3.innerHTML === 'O') || (b1.innerHTML === 'O' && b2.innerHTML === 'O' && b3.innerHTML === 'O') || (c1.innerHTML === 'O' && c2.innerHTML === 'O' && c3.innerHTML === 'O') || (a1.innerHTML === 'O' && b2.innerHTML === 'O' && c3.innerHTML === 'O') || (a3.innerHTML === 'O' && b2.innerHTML === 'O' && c1.innerHTML === 'O') || (a1.innerHTML === 'O' && b1.innerHTML === 'O' && c1.innerHTML === 'O') || (a2.innerHTML === 'O' && b2.innerHTML === 'O' && c2.innerHTML === 'O') || (a3.innerHTML === 'O' && b3.innerHTML === 'O' && c3.innerHTML === 'O')
+                      )
+
+                      { 
+                        document.getElementById("reset").style.opacity = 1;
+                        alert('Comp wins!'); 
+                        boardTds.forEach(function(x) {
+                            x.classList.add("taken");
+                        }) 
+                      }
+
+                 else { }
+
+                 }   
+              
+             // if picked random fields is taken start function again
+             else {
+                compPick(); 
+             }
+       } 
 });  
