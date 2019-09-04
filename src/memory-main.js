7955b92e9867583671f74a2a6edf4e568d7ff157
Andrzej Korbiel <andrzej.korbiel@Lumesse.com>
2019-09-04 14:56:07 +0200

    Refactoring pt.1

--- src/memory-main.js
+++ src/memory-main.js
@@ -1,53 +1,55 @@
-$(document).ready(function() {
+let icons = [
+    'fa-anchor', 'fa-bell', 'fa-bomb', 'fa-camera', 'fa-cog', 'fa-dice', 'fa-binoculars', 'fa-award', 'fa-bath'
+];
 
-  // generate random board
-  let cards = ['<i class="fas fa-anchor"></i>', '<i class="fas fa-anchor"></i>', '<i class="fas fa-bell"></i>', '<i class="fas fa-bell"></i>', '<i class="fas fa-bomb"></i>', '<i class="fas fa-bomb"></i>', '<i class="fas fa-camera"></i>', '<i class="fas fa-camera"></i>', '<i class="fas fa-cog"></i>', '<i class="fas fa-cog"></i>', '<i class="fas fa-dice"></i>', '<i class="fas fa-dice"></i>', '<i class="far fa-eye"></i>', '<i class="far fa-eye"></i>','<i class="fas fa-fighter-jet"></i>', '<i class="fas fa-fighter-jet"></i>', '<i class="far fa-gem"></i>', '<i class="far fa-gem"></i>'];
-  
-  // shuffle elements in arrray without repetation
-  function shuffle(cards) {
+let cards = [];
+let compare = [];
+let saved = [];
+let score_1 = 0;
+let score_2 = 0;
+
+function generateCards(icon) {
+  for (let x = 0; x<=1; x++) {
+    cards.push({element: '<i class="fas ' + icon + '"></i>', label: icon})
+  }
+}
+
+function shuffle(cards) {
     var m = cards.length, t, i;
     while (m > 0)   {
-      i = Math.floor(Math.random() * m--);
-      t = cards[m];
-      cards[m] = cards[i];
-      cards[i] = t;
+        i = Math.floor(Math.random() * m--);
+        t = cards[m];
+        cards[m] = cards[i];
+        cards[i] = t;
     }
     return cards;
-  }
+}
 
-  shuffle(cards); 
-
-  // put shuffled elements from array on board
-  function randomCards() {
+function displayCards() {
     let elements = '';
     for (let n = 0; n < cards.length; n++) {
- 
-      elements += `<div id="${n}" class="square" value='${cards[n]}'>${cards[n]}</div>` // add new element with specify id and value
-      
+        elements += `<div id="${n}" class="square" value='${cards[n].label}'>${cards[n].element}</div>`
     }
     $('#table').html(elements);
-  }
-  
-  randomCards();
-  
-  // main function - cards comparing
-  let compare = [];
-  let saved = [];
-  let score_1 = 0;
-  let score_2 = 0;
-  
+}
+
+icons.forEach(generateCards);
+shuffle(cards);
+displayCards();
+
+
+$(document).ready(function() {
+
   $('.square').click(function() {
-  
-    // show clicked cards
+
     $(this).toggleClass('show flipInY');
 
-    // push clicked cards into array to compare them
     compare.push(($(this).attr('value')));
     saved.push(($(this).attr('id')));
 
     // on first click
-    if (compare.length < 2) { } 
-    
+    if (compare.length < 2) { }
+
     // on second click
     else if (compare.length == 2) {
 
@@ -68,18 +70,18 @@ $(document).ready(function() {
         score_1++;
         $('#score-1').html(`Pairs find out: ${score_1}`);
 
-        if (score_1 == 9) { 
-          alert('End of the game'); 
+        if (score_1 == 9) {
+          alert('End of the game');
           document.getElementById("reset").style.display = "block";
         };
 
-      } 
+      }
 
       else {
          // hide cards that wasn't matched
         setTimeout(function () {
                     $('.square').removeClass('show');
-        }, 1200); 
+        }, 1200);
 
         // clear both arrays
         compare = [];
@@ -87,13 +89,10 @@ $(document).ready(function() {
         // add 1 to missed clicks
         score_2++;
         $('#score-2').html(`Missed clicks: ${score_2}`);
-   
+
       }
 
-    } 
-    
-    else { }
-
+    }
   })
 
 });
